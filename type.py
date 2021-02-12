007b0ca05703789736c993cab2fbfd84f62e8b2d
root <rohini@nubo-lab.com>
2021-02-12 17:18:12 +0000

    Adding type of object

---
+++ type.py
@@ -0,0 +1,16 @@
+# Determine which class a given Bus object belongs to (Check type of a object)
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+## use Python's built-in type() function
+print(type(School_bus))
+
